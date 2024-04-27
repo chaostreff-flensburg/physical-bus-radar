7c5ed50cf4032f6216636388c6d87a3520204e03
Alexander E <alex@eichhorn.dev>
2024-04-27 19:40:43 +0000

    strandtest demo + fix led pin

--- busradar.py
+++ busradar.py
@@ -1,9 +1,11 @@
 import requests
+import random
+import time
 from rpi_ws281x import PixelStrip, Color
 
 # LED-Konfiguration
 LED_COUNT = 65  # Anzahl der LEDs
-LED_PIN = 12    # GPIO-Pin für den LED-Streifen
+LED_PIN = 18    # GPIO-Pin für den LED-Streifen
 LED_FREQ_HZ = 800000  # Frequenz (Hz) für den LED-Streifen
 LED_DMA = 10    # DMA-Kanal für den LED-Streifen
 LED_BRIGHTNESS = 255  # Helligkeit (0-255) für den LED-Streifen
@@ -27,19 +29,23 @@ def get_led_data():
 def control_leds(led_data):
     # Setup LED strip
     # Schleife durch die LED-Daten und setzen der Farben
+    #print(led_data)
     for led_info in led_data:
         led_index = int(led_info['led'])
         color_str = led_info['color']
         color_int = Color(*(int(color_str[i:i+2], 16) for i in (1, 3, 5)))  # Umwandlung von Hexadezimalfarbe in Farbnummer
         strip.setPixelColor(led_index, color_int)
 
-    # Aktualisierung der LED-Streifen
-    strip.show()
-
 if __name__ == "__main__":
     strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
     strip.begin()
+    resetrange = range(0, LED_COUNT)
     while True:
+        for ledi in resetrange:
+            colorrandom = random.randint(4,10)
+            strip.setPixelColor(ledi, Color(colorrandom,colorrandom,colorrandom))
         led_data = get_led_data()
         if led_data:
             control_leds(led_data)
+        time.sleep(0.1)
+        strip.show()
